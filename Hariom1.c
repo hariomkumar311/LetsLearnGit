a9077f13f100189c5b53610a64e7ebe0d7e37486
hariomkumar311 <hariomk.ug20.ece@nitp.ac.in>
2021-09-10 18:43:19 +0530

    Initial commit

---
+++ Hariom1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+int arr[10],i;
+printf("Enter array elements\n");
+for(i=0;i<=9;i++){
+
+scanf("%d",&arr[i]);
+
+}
+printf("Entered array elements are\n");
+for(i=0;i<=9;i++){
+	printf("%d\n",arr[i]);
+}
+int small=arr[0];
+int large=arr[0];
+for(i=0;i<=9;i++){
+	if(arr[i]>large)
+	large=arr[i];
+	if(arr[i]<small)
+	small=arr[i];
+	
+
+}
+	printf("Largest element is %d\n",large);
+   	printf("smallest element is %d",small);
+}
